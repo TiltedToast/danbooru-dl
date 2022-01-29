74b62bd183084802c0a4b63b8d17a7e0eee8a82a
TiltedToast <timdortmann@gmail.com>
2022-01-29 04:14:41 +0100

    full multithreading support

--- main.py
+++ main.py
@@ -24,10 +24,12 @@ def resource_path(relative_path: str) -> str:
 
 driver_path = "driver/chromedriver.exe"
 BASE_URL = "http://danbooru.donmai.us"
+posts = []
 
 
 def download_url(url: str, save_location: str):
-    r = requests.get(url, stream=True, timeout=10, headers={'User-agent': 'Mozilla/5.0'})
+    r = requests.get(url, stream=True, timeout=10,
+                     headers={'User-agent': 'Mozilla/5.0'})
     with open(save_location, 'wb') as f:
         r.raw.decode_content = True
         shutil.copyfileobj(r.raw, f)
@@ -47,21 +49,47 @@ class DownloadWorker(Thread):
                 self.queue.task_done()
 
 
-def add_posts_to_list(page_amount_total, tag, safe, risky, explicit):
-    j = 0
-    posts = []
-    for i in range(int(page_amount_total)):
-        r = requests.get(f"{BASE_URL}/posts.json?page={i + 1}&tags={tag}")
-        if r.status_code == 200:
-            data = json.loads(r.text)
-            for post in data:
-                if post not in posts:
-                    if (safe and post['rating'] == "s") or (risky and post['rating'] == "q") or \
-                            (explicit and post['rating'] == "e"):
-                        posts.append(post)
-                        click.echo(f"Added Post #{j + 1} to the queue")
-                        j += 1
+class DownloadWorker2(Thread):
+    global posts
 
+    def __init__(self, queue2: Queue):
+        Thread.__init__(self)
+        self.queue = queue2
+
+    def run(self):
+
+        while True:
+            i, tag, safe, risky, explicit = self.queue.get()
+            try:
+                r = requests.get(f"{BASE_URL}/posts.json?page={i + 1}&tags={tag}")
+                if r.status_code == 200:
+                    data = json.loads(r.text)
+                    for post in data:
+                        if post not in posts:
+                            if (safe and post['rating'] == "s") or (risky and post['rating'] == "q") or \
+                                    (explicit and post['rating'] == "e"):
+                                posts.append(post)
+            finally:
+                self.queue.task_done()
+
+
+def add_posts_to_list(page_amount_total, tag, safe, risky, explicit):
+    global posts
+
+    thread_count = multiprocessing.cpu_count()
+    queue = Queue()
+
+    for k in range(thread_count):
+        worker = DownloadWorker2(queue)
+        worker.daemon = True
+        worker.start()
+
+    j = 0
+    for i in range(int(page_amount_total)):
+        queue.put((i, tag, safe, risky, explicit))
+        j += 1
+
+    queue.join()
     return posts
 
 
@@ -76,10 +104,11 @@ def main(tag: str, output: str = "output", safe: str = True, risky=False, explic
     """
     Search Danbooru for images with a given tag and download them to the output directory
     """
+    start_time = time.time()
 
     r = requests.get(BASE_URL + f"/posts?tags={quote_plus(tag)}")
     soup = BeautifulSoup(r.text, "html.parser")
-    
+
     navbar_nums = soup.find_all("a", class_="paginator-page desktop-only")
 
     try:
@@ -90,6 +119,7 @@ def main(tag: str, output: str = "output", safe: str = True, risky=False, explic
 
     os.mkdir(output) if not os.path.isdir(output) else None
 
+    click.echo(f"Found {page_amount_total} pages worth of posts, adding to queue...")
     posts = add_posts_to_list(page_amount_total, tag, safe, risky, explicit)
     thread_count = multiprocessing.cpu_count()
     queue = Queue()
@@ -105,7 +135,8 @@ def main(tag: str, output: str = "output", safe: str = True, risky=False, explic
                 if not os.path.isdir(f"{output}/safe"):
                     os.mkdir(f"{output}/safe")
 
-                queue.put((post["file_url"], f"{output}/safe/{post['id']}.{post['file_ext']}"))
+                queue.put(
+                    (post["file_url"], f"{output}/safe/{post['id']}.{post['file_ext']}"))
                 # click.echo(f"[SAFE] Downloaded {post['id']}.{post['file_ext']}")
 
             elif risky and post["rating"] == "q" and not os.path.exists(
@@ -113,7 +144,8 @@ def main(tag: str, output: str = "output", safe: str = True, risky=False, explic
                 if not os.path.isdir(f"{output}/risky"):
                     os.mkdir(f"{output}/risky")
 
-                queue.put((post["file_url"], f"{output}/risky/{post['id']}.{post['file_ext']}"))
+                queue.put(
+                    (post["file_url"], f"{output}/risky/{post['id']}.{post['file_ext']}"))
                 # click.echo(f"[RISKY] Downloaded {post['id']}.{post['file_ext']}")
 
             elif explicit and post["rating"] == "e" and not os.path.exists(
@@ -121,13 +153,13 @@ def main(tag: str, output: str = "output", safe: str = True, risky=False, explic
                 if not os.path.isdir(f"{output}/explicit"):
                     os.mkdir(f"{output}/explicit")
 
-                queue.put((post["file_url"], f"{output}/explicit/{post['id']}.{post['file_ext']}"))
+                queue.put(
+                    (post["file_url"], f"{output}/explicit/{post['id']}.{post['file_ext']}"))
                 # click.echo(f"[EXPLICIT] Downloaded {post['id']}.{post['file_ext']}")
         except KeyError:
             continue
 
-    click.echo("Downloading...")
-    start_time = time.time()
+    click.echo(f"Downloading {len(posts)} posts...")
     queue.join()
     click.echo(f"Finished downloading after {round(time.time() - start_time, 2)} seconds")
 
